110dced7bd2f936ff8fbe2ab23f18992d284fba9
trin-daniel <trindadedanniel63@outlook.com>
2020-11-23 15:26:23 -0300

    test: ensure AddOrphanageController call the validation with the correct values

---
+++ src/presentation/controllers/orphanage/add-orphanage/add-orphanage-controller.spec.ts
@@ -0,0 +1,37 @@
+import { AddOrphanageController } from './add-orphanage-controller'
+import { Validation } from '../../../protocols/validation'
+import { address, internet, random } from 'faker/locale/pt_BR'
+
+const httpRequest = {
+  body:
+   {
+     name: internet.userName(),
+     latitude: Number(address.latitude()),
+     longitude: Number(address.longitude()),
+     about: random.words(5),
+     instructions: random.words(5),
+     opening_hours: new Date(),
+     closing_time: new Date(),
+     open_on_weekends: random.boolean()
+   }
+}
+
+const mockValidation = (): Validation => {
+  class ValidationSpy implements Validation {
+    validate (input: object): Error | null {
+      return null
+    }
+  }
+  return new ValidationSpy()
+}
+
+describe('Add Orphanage Controller', () => {
+  test('Should be able to call the validation with the correct values', () => {
+    const validationSpy = mockValidation()
+    const sut = new AddOrphanageController(validationSpy)
+    const validateSpy = jest.spyOn(validationSpy, 'validate')
+    const request = httpRequest
+    sut.handle(request)
+    expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
+})
