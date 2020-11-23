49a3e86f26e7aaeed1f8c8dfae3ab2b74b8d5774
trin-daniel <trindadedanniel63@outlook.com>
2020-11-23 18:16:27 -0300

    test: ensure AddOrphanageController call AddOrphanage with the correct values

--- src/presentation/controllers/orphanage/add-orphanage/add-orphanage-controller.spec.ts
+++ src/presentation/controllers/orphanage/add-orphanage/add-orphanage-controller.spec.ts
@@ -2,6 +2,9 @@ import { AddOrphanageController } from './add-orphanage-controller'
 import { Validation } from '../../../protocols/validation'
 import { address, internet, random } from 'faker/locale/pt_BR'
 import { badRequest } from '../../../helpers/http-helpers'
+import { Orphanage } from '../../../../domain/models/orphanage'
+import { AddOrphanage, AddOrphanageArgs } from '../../../../domain/use-cases/orphanage/add-orphanage'
+
 const httpRequest = {
   body:
    {
@@ -16,6 +19,8 @@ const httpRequest = {
    }
 }
 
+const mockOrphanage = { ...httpRequest.body, id: random.uuid() }
+
 const mockValidation = (): Validation => {
   class ValidationSpy implements Validation {
     validate (input: object): Error | null {
@@ -25,12 +30,22 @@ const mockValidation = (): Validation => {
   return new ValidationSpy()
 }
 
-type SutTypes = { sut: AddOrphanageController, validationSpy: Validation }
+const mockAddOrphanage = (): AddOrphanage => {
+  class AddOrphanageSpy implements AddOrphanage {
+    async add (data: AddOrphanageArgs): Promise<Orphanage> {
+      return Promise.resolve(mockOrphanage)
+    }
+  }
+  return new AddOrphanageSpy()
+}
+
+type SutTypes = { sut: AddOrphanageController, validationSpy: Validation, addOrphanageSpy: AddOrphanage }
 
 const makeSut = (): SutTypes => {
+  const addOrphanageSpy = mockAddOrphanage()
   const validationSpy = mockValidation()
-  const sut = new AddOrphanageController(validationSpy)
-  return { sut, validationSpy }
+  const sut = new AddOrphanageController(validationSpy, addOrphanageSpy)
+  return { sut, validationSpy, addOrphanageSpy }
 }
 
 describe('Add Orphanage Controller', () => {
@@ -48,5 +63,13 @@ describe('Add Orphanage Controller', () => {
     const request = httpRequest
     const response = await sut.handle(request)
     expect(response).toEqual(badRequest(new Error()))
+  })
+
+  test('Should be able to call AddOrphanage with the correct values', async () => {
+    const { sut, addOrphanageSpy } = makeSut()
+    const addSpy = jest.spyOn(addOrphanageSpy, 'add')
+    const request = httpRequest
+    await sut.handle(request)
+    expect(addSpy).toHaveBeenCalledWith(request.body)
   })
 })
