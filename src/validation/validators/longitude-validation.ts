4c8cc8b1bdb322d9bc08947dba661ce332b7b596
trin-daniel <trindadedanniel63@outlook.com>
2020-11-23 23:05:32 -0300

    feat: ensure LongitudeValidation return a InvalidParamError if validation fails

---
+++ src/validation/validators/longitude-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '@presentation/errors'
+import { Validation } from '@presentation/protocols'
+import { CoordinateValidator } from '@validation/protocols/coordinate-validator'
+
+export class LongitudeValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly coordinateValidator: CoordinateValidator
+  ) {}
+
+  validate (input: object): Error | null {
+    const isCoordinate = this.coordinateValidator.isCoordinate(input[this.fieldName])
+    if (!isCoordinate) {
+      return new InvalidParamError(this.fieldName)
+    }
+  }
+}
