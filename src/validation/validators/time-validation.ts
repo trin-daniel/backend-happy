1c7fc97658a0589546b19092d6b789ad6f8677e1
trin-daniel <trindadedanniel63@outlook.com>
2020-11-24 20:50:19 -0300

    feat: ensure TimeValidation return a InvalidParamError if validation fails

---
+++ src/validation/validators/time-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '@presentation/errors'
+import { Validation } from '@presentation/protocols'
+import { TimeValidator } from '@validation/protocols/time-validator'
+
+export class TimeValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly timeValidator: TimeValidator
+  ) {}
+
+  validate (input: object): Error | null {
+    const isTime = this.timeValidator.isTime(input[this.fieldName])
+    if (!isTime) {
+      return new InvalidParamError(this.fieldName)
+    }
+  }
+}
