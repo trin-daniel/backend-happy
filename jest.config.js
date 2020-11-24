9c70d15cd13341a304f0ee0c64ce70da7efdec73
trin-daniel <trindadedanniel63@outlook.com>
2020-11-23 23:13:14 -0300

    chore: ignore protocols folder in tests coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ const { compilerOptions } = require('./tsconfig.json')
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
 
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   coverageDirectory: 'coverage',
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
