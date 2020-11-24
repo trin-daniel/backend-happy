d76438fe41b0b51c454a7a0bd3ba6b9fed35ad4e
trin-daniel <trindadedanniel63@outlook.com>
2020-11-23 22:23:54 -0300

    feat: ensure RequiredFieldValidation return a MissingParamError if validation fails

---
+++ src/validation/validators/required-field-validation.ts
@@ -0,0 +1,15 @@
+import { MissingParamError } from '@presentation/errors'
+import { Validation } from '@presentation/protocols'
+
+export class RequiredFieldValidation implements Validation {
+  constructor (
+    private readonly fieldName: string
+  ) {}
+
+  validate (input: object): Error | null {
+    const isFilled = input[this.fieldName]
+    if (!isFilled) {
+      return new MissingParamError(this.fieldName)
+    }
+  }
+}
