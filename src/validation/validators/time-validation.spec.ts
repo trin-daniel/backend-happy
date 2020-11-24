bd290e91385f06f5bdf579bffcc8cc0767268945
trin-daniel <trindadedanniel63@outlook.com>
2020-11-24 20:50:32 -0300

    test: ensure TimeValidation return a InvalidParamError if validation fails

---
+++ src/validation/validators/time-validation.spec.ts
@@ -0,0 +1,23 @@
+import { InvalidParamError } from '@presentation/errors'
+import { TimeValidation } from '@validation/validators/time-validation'
+import { TimeValidator } from '@validation/protocols/time-validator'
+import { time } from 'faker'
+
+const mockTimeValidator = (): TimeValidator => {
+  class TimeValidatorSpy implements TimeValidator {
+    isTime (value: number): boolean {
+      return true
+    }
+  }
+  return new TimeValidatorSpy()
+}
+
+describe('Time Validation', () => {
+  test('Should be able to return a InvalidParamError if validation fails', () => {
+    const timeValidatorSpy = mockTimeValidator()
+    const sut = new TimeValidation('field', timeValidatorSpy)
+    jest.spyOn(timeValidatorSpy, 'isTime').mockReturnValueOnce(false)
+    const error = sut.validate({ field: time.recent() })
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+})
