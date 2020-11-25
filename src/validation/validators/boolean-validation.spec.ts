eeca72edfae7770392a82bee67f00a78866adf0d
trin-daniel <trindadedanniel63@outlook.com>
2020-11-25 13:25:42 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/boolean-validation.spec.ts
+++ src/validation/validators/boolean-validation.spec.ts
@@ -2,16 +2,27 @@ import { InvalidParamError } from '@presentation/errors'
 import { BooleanValidation } from '@validation/validators/boolean-validation'
 import { random } from 'faker'
 
+type SutTypes = { sut: BooleanValidation }
+
+const makeSut = (): SutTypes => {
+  const sut = new BooleanValidation('field')
+  return {
+    sut
+  }
+}
+
 describe('Boolean Validation', () => {
   test('Should be able to return a InvalidParamError if validation fails', () => {
-    const sut = new BooleanValidation('field')
-    const error = sut.validate({ field: random.word() })
+    const { sut } = makeSut()
+    const data = { field: random.word() }
+    const error = sut.validate(data)
     expect(error).toEqual(new InvalidParamError('field'))
   })
 
   test('Should be able to return null if validation is successful', () => {
-    const sut = new BooleanValidation('field')
-    const error = sut.validate({ field: random.boolean() })
+    const { sut } = makeSut()
+    const data = { field: random.boolean() }
+    const error = sut.validate(data)
     expect(error).toBeNull()
   })
 })
