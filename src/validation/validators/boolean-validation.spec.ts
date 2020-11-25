a98cb0f49b346d2bb12f8ffd8d2f88db3835f2b9
trin-daniel <trindadedanniel63@outlook.com>
2020-11-25 13:18:55 -0300

    test: ensure BooleanValidation return a InvalidParamError if validation fails

---
+++ src/validation/validators/boolean-validation.spec.ts
@@ -0,0 +1,11 @@
+import { InvalidParamError } from '@presentation/errors'
+import { BooleanValidation } from '@validation/validators/boolean-validation'
+import { random } from 'faker'
+
+describe('Boolean Validation', () => {
+  test('Should be able to return a InvalidParamError if validation fails', () => {
+    const sut = new BooleanValidation('field')
+    const error = sut.validate({ field: random.word() })
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+})
