19587db07ae68328b3e9fc99917543381a84d8dd
trin-daniel <trindadedanniel63@outlook.com>
2020-11-25 13:18:42 -0300

    feat: ensure BooleanValidation return a InvalidParamError if validation fails

---
+++ src/validation/validators/boolean-validation.ts
@@ -0,0 +1,15 @@
+import { InvalidParamError } from '@presentation/errors'
+import { Validation } from '@presentation/protocols'
+
+export class BooleanValidation implements Validation {
+  constructor (
+    private readonly fieldName: string
+  ) {}
+
+  validate (input: object): Error | null {
+    const value = input[this.fieldName]
+    if (typeof value !== 'boolean') {
+      return new InvalidParamError(this.fieldName)
+    }
+  }
+}
