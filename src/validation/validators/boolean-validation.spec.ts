15f507e39ef230c30f2b356fac52b344e02077c4
trin-daniel <trindadedanniel63@outlook.com>
2020-11-25 13:22:28 -0300

    test: ensure BooleanValidation return null if validation is successful

--- src/validation/validators/boolean-validation.spec.ts
+++ src/validation/validators/boolean-validation.spec.ts
@@ -8,4 +8,10 @@ describe('Boolean Validation', () => {
     const error = sut.validate({ field: random.word() })
     expect(error).toEqual(new InvalidParamError('field'))
   })
+
+  test('Should be able to return null if validation is successful', () => {
+    const sut = new BooleanValidation('field')
+    const error = sut.validate({ field: random.boolean() })
+    expect(error).toBeNull()
+  })
 })
