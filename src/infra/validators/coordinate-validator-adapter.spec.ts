2f0287055c4292bae8174cf298db8decdb6bfd2e
trin-daniel <trindadedanniel63@outlook.com>
2020-11-26 19:47:36 -0300

    refactor: move sut creation to a factory method

--- src/infra/validators/coordinate-validator-adapter.spec.ts
+++ src/infra/validators/coordinate-validator-adapter.spec.ts
@@ -8,9 +8,18 @@ jest.mock('validator', () => ({
   }
 }))
 
+type SutTypes = { sut: CoordinateValidatorAdapter }
+
+const makeSut = (): SutTypes => {
+  const sut = new CoordinateValidatorAdapter()
+  return {
+    sut
+  }
+}
+
 describe('Coordinate Validator Adapter', () => {
   test('Should be able to call the validator with the correct value', () => {
-    const sut = new CoordinateValidatorAdapter()
+    const { sut } = makeSut()
     const isLatLongSpy = jest.spyOn(validator, 'isLatLong')
     const value = Number(address.latitude())
     sut.isCoordinate(value)
@@ -18,7 +27,7 @@ describe('Coordinate Validator Adapter', () => {
   })
 
   test('Should be able to return false if the validator returns false', () => {
-    const sut = new CoordinateValidatorAdapter()
+    const { sut } = makeSut()
     jest.spyOn(validator, 'isLatLong').mockReturnValueOnce(false)
     const value = Number(address.latitude())
     const isCoordinate = sut.isCoordinate(value)
@@ -26,7 +35,7 @@ describe('Coordinate Validator Adapter', () => {
   })
 
   test('Should be able to return true if the validator returns true', () => {
-    const sut = new CoordinateValidatorAdapter()
+    const { sut } = makeSut()
     const value = Number(address.latitude())
     const isCoordinate = sut.isCoordinate(value)
     expect(isCoordinate).toBe(true)
