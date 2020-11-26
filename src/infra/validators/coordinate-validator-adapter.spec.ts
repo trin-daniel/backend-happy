afb68a05d448b83de8fc35d3cce889d435e340b7
trin-daniel <trindadedanniel63@outlook.com>
2020-11-26 19:43:50 -0300

    test: ensure CoordinateValidatorAdapter return false if the validator returns false

--- src/infra/validators/coordinate-validator-adapter.spec.ts
+++ src/infra/validators/coordinate-validator-adapter.spec.ts
@@ -16,4 +16,12 @@ describe('Coordinate Validator Adapter', () => {
     sut.isCoordinate(value)
     expect(isLatLongSpy).toHaveBeenCalledWith(String(value))
   })
+
+  test('Should be able to return false if the validator returns false', () => {
+    const sut = new CoordinateValidatorAdapter()
+    jest.spyOn(validator, 'isLatLong').mockReturnValueOnce(false)
+    const value = Number(address.latitude())
+    const isCoordinate = sut.isCoordinate(value)
+    expect(isCoordinate).toBe(false)
+  })
 })
