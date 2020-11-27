437b727883feff7d35a8897aff3cebf530cebc96
trin-daniel <trindadedanniel63@outlook.com>
2020-11-27 11:43:00 -0300

    feat: ensure CorsMiddleware enable cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { BodyParser } from '@main/middlewares/body-parser'
 import { ContentType } from '@main/middlewares/content-type'
+import { Cors } from '@main/middlewares/cors-middleware'
 import { Express } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(BodyParser)
   app.use(ContentType)
+  app.use(Cors)
 }
