072e3a4a3ba86f3e4fcd4f1d4d82a7a4537427ee
trin-daniel <trindadedanniel63@outlook.com>
2020-11-27 16:42:39 -0300

    feat: ensure NoCacheMiddleware disable the cache

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
-import { BodyParserMiddleware, CorsMiddleware, ContentTypeMiddleware } from '@main/middlewares'
+import { BodyParserMiddleware, CorsMiddleware, ContentTypeMiddleware, noCacheMiddleware } from '@main/middlewares'
 import { Express } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(BodyParserMiddleware)
   app.use(ContentTypeMiddleware)
   app.use(CorsMiddleware)
+  app.use(noCacheMiddleware)
 }
