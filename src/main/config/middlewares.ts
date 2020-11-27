f8a09b2410814f7249e874070eb17e5b369bc614
trin-daniel <trindadedanniel63@outlook.com>
2020-11-27 11:27:02 -0300

    feat: ensure ContentType return JSON format by default

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { BodyParser } from '@main/middlewares/body-parser'
+import { ContentType } from '@main/middlewares/content-type'
 import { Express } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(BodyParser)
+  app.use(ContentType)
 }
