87a8937071748ce755190788ee65c4f59697399c
trin-daniel <trindadedanniel63@outlook.com>
2020-11-26 21:13:09 -0300

    refactor: move sut creation to a factory method

--- src/infra/validators/date-validator-adapter.spec.ts
+++ src/infra/validators/date-validator-adapter.spec.ts
@@ -10,9 +10,18 @@ jest.mock('moment', () => {
   }
 })
 
+type SutTypes = { sut: DateValidatorAdapter }
+
+const makeSut = (): SutTypes => {
+  const sut = new DateValidatorAdapter()
+  return {
+    sut
+  }
+}
+
 describe('Date Validator Adapter', () => {
   test('Should be able to call the validator with the correct value', () => {
-    const sut = new DateValidatorAdapter()
+    const { sut } = makeSut()
     const isDateSpy = jest.spyOn(moment, 'isDate')
     const value = time.recent()
     const date = new Date(value)
@@ -21,7 +30,7 @@ describe('Date Validator Adapter', () => {
   })
 
   test('Should be able to return false if the validator returns false', () => {
-    const sut = new DateValidatorAdapter()
+    const { sut } = makeSut()
     jest.spyOn(moment, 'isDate').mockReturnValueOnce(false)
     const value = time.recent()
     const isDate = sut.isDate(value)
@@ -29,7 +38,7 @@ describe('Date Validator Adapter', () => {
   })
 
   test('Should be able to return true if the validator returns true', () => {
-    const sut = new DateValidatorAdapter()
+    const { sut } = makeSut()
     const value = time.recent()
     const isDate = sut.isDate(value)
     expect(isDate).toBe(true)
