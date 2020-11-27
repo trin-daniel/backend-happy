9fcd221ca34dd9f0a33a210620324eab7228e9bd
trin-daniel <trindadedanniel63@outlook.com>
2020-11-27 17:17:32 -0300

    test: ensure AddOrphanageRoute return 200 if the orphanage is successfully added

---
+++ src/main/routes/add-orphanage-route.test.ts
@@ -0,0 +1,29 @@
+import { AddOrphanageArgs } from '@domain/use-cases/orphanage/add-orphanage'
+import app from '@main/config/app'
+import request from 'supertest'
+import { random, internet, address, time } from 'faker/locale/pt_BR'
+import { SqlHelper } from '@infra/database/helpers/sql-helper'
+
+const inputs: AddOrphanageArgs = {
+  name: internet.userName(),
+  latitude: Number(address.latitude()),
+  longitude: Number(address.longitude()),
+  about: random.words(5),
+  instructions: random.words(5),
+  opening_hours: time.recent(),
+  closing_time: time.recent(),
+  open_on_weekends: random.boolean()
+}
+
+describe('Add Orphanage Route', () => {
+  beforeAll(async () => await SqlHelper.connect())
+  afterAll(async () => await SqlHelper.disconnect())
+  beforeEach(async () => await SqlHelper.delete('DELETE FROM orphanages'))
+
+  test('Should be able to return 200 if the orphanage is successfully added', async () => {
+    await request(app)
+      .post('/api/add-orphanage')
+      .send(inputs)
+      .expect(200)
+  })
+})
