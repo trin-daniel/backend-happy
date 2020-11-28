3a24404e0f502d1306cf8e8202843393c7e86dbc
trin-daniel <trindadedanniel63@outlook.com>
2020-11-28 16:57:25 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express/express-route-adapter.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpRequest } from '@presentation/protocols'
+import { Request, Response } from 'express'
+
+export const ExpressRouteAdapter = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest<any> = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res.status(httpResponse.statusCode).json({ Error: httpResponse.body.message })
+    }
+  }
+}
