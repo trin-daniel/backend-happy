c7e5e35af141d3d763f85572b632abd2c71c93a5
trin-daniel <trindadedanniel63@outlook.com>
2020-12-02 01:07:16 -0300

    test: ensure DbLoadAllOrphanages return a list of orphanages if successful

--- src/data/use-cases/orphanage/load-all-orphanages/db-load-all-orphanages.spec.ts
+++ src/data/use-cases/orphanage/load-all-orphanages/db-load-all-orphanages.spec.ts
@@ -1,11 +1,24 @@
 import { DbLoadAllOrphanages } from '@data/use-cases/orphanage/load-all-orphanages/db-load-all-orphanages'
 import { LoadAllOrphanagesRepository } from '@data/protocols/load-all-orphanages-repository'
 import { Orphanage } from '@domain/models/orphanage'
+import { internet, random, time, address } from 'faker/locale/pt_BR'
+
+const mockOrphanage: Orphanage[] = [{
+  id: random.uuid(),
+  name: internet.userName(),
+  latitude: Number(address.latitude()),
+  longitude: Number(address.longitude()),
+  about: random.words(5),
+  instructions: random.words(5),
+  opening_hours: time.recent(),
+  closing_time: time.recent(),
+  open_on_weekends: random.boolean()
+}]
 
 const mockLoadAllOrphanagesRepository = (): LoadAllOrphanagesRepository => {
   class LoadAllOrphanagesRepositorySpy implements LoadAllOrphanagesRepository {
     async load (): Promise<Orphanage[]> {
-      return Promise.resolve(null)
+      return Promise.resolve(mockOrphanage)
     }
   }
   return new LoadAllOrphanagesRepositorySpy()
@@ -18,5 +31,12 @@ describe('DbLoadAllOrphanages Usecase', () => {
     const loadSpy = jest.spyOn(loadAllOrphanagesRepositorySpy, 'load')
     await sut.loadAll()
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should be able to return a list of orphanages if successful', async () => {
+    const loadAllOrphanagesRepositorySpy = mockLoadAllOrphanagesRepository()
+    const sut = new DbLoadAllOrphanages(loadAllOrphanagesRepositorySpy)
+    const orphanages = await sut.loadAll()
+    expect(orphanages).toEqual(mockOrphanage)
   })
 })
