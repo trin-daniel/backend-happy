13a9610cbb02bc62d9c8f84c84d819bed0d13c8b
trin-daniel <trindadedanniel63@outlook.com>
2020-12-03 09:30:17 -0300

    refactor: move sut creation to a factory method

--- src/infra/database/repositories/orphanage/orphanage-repository.spec.ts
+++ src/infra/database/repositories/orphanage/orphanage-repository.spec.ts
@@ -14,13 +14,22 @@ const orphanageArgs: AddOrphanageArgs = {
   open_on_weekends: random.boolean()
 }
 
+type SutTypes = { sut: OrphanageRepository }
+
+const makeSut = (): SutTypes => {
+  const sut = new OrphanageRepository()
+  return {
+    sut
+  }
+}
+
 describe('Orphanage Repository', () => {
   beforeAll(async () => await SqlHelper.connect())
   afterAll(async () => await SqlHelper.disconnect())
   beforeEach(async () => await SqlHelper.delete('DELETE FROM orphanages'))
 
   test('Should be able to add a new orphanage to the database', async () => {
-    const sut = new OrphanageRepository()
+    const { sut } = makeSut()
     const orphanage = await sut.add(orphanageArgs)
     expect(orphanage).toBeTruthy()
     expect(orphanage.id).toBeTruthy()
@@ -31,13 +40,13 @@ describe('Orphanage Repository', () => {
     await SqlHelper.insertOne('INSERT INTO orphanages (id, name, latitude, longitude, about, instructions, opening_hours, closing_time, open_on_weekends)  VALUES (?,?,?,?,?,?,?,?,?)',
       [id, orphanageArgs.name, orphanageArgs.latitude, orphanageArgs.longitude, orphanageArgs.about, orphanageArgs.instructions, orphanageArgs.opening_hours, orphanageArgs.closing_time, orphanageArgs.open_on_weekends]
     )
-    const sut = new OrphanageRepository()
+    const { sut } = makeSut()
     const orphanages = await sut.load()
     expect(orphanages).toBeTruthy()
   })
 
   test('Should be able to return an empty matrix if there is no data in the table', async () => {
-    const sut = new OrphanageRepository()
+    const { sut } = makeSut()
     const orphanages = await sut.load()
     expect(orphanages).toEqual([])
   })
