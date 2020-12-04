5d99c42f1b3578036f4fdadd60361ed3f3d0e647
trin-daniel <trindadedanniel63@outlook.com>
2020-12-04 20:48:35 -0300

    feat: ensure LoadOrphanageController return an orphanage if successful

--- src/presentation/controllers/orphanage/load-orphanage/load-orphanage-controller.ts
+++ src/presentation/controllers/orphanage/load-orphanage/load-orphanage-controller.ts
@@ -1,13 +1,15 @@
 import { LoadOrphanage } from '@domain/use-cases/orphanage/load-orphanage'
+import { ok } from '@presentation/helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols'
+import { Orphanage } from '../add-orphanage/add-orphanage-controller-protocols'
 
 export class LoadOrphanageController implements Controller {
   constructor (
     private readonly loadOrphanage: LoadOrphanage
   ) {}
 
-  async handle (req: HttpRequest<any>): Promise<HttpResponse<any>> {
-    await this.loadOrphanage.loadById(req.params.id)
-    return Promise.resolve(null)
+  async handle (req: HttpRequest<any>): Promise<HttpResponse<Orphanage | Error>> {
+    const orphanage = await this.loadOrphanage.loadById(req.params.id)
+    return ok(orphanage)
   }
 }
