6942d938170a71f925d781d6b802553d5e1dcdb3
trin-daniel <trindadedanniel63@outlook.com>
2020-12-04 20:42:52 -0300

    test: ensure LoadOrphanageController call LoadOrphanage with the correct value

---
+++ src/presentation/controllers/orphanage/load-orphanage/load-orphanage-controller.spec.ts
@@ -0,0 +1,42 @@
+import { Orphanage } from '@domain/models/orphanage'
+import { LoadOrphanage } from '@domain/use-cases/orphanage/load-orphanage'
+import { LoadOrphanageController } from '@presentation/controllers/orphanage/load-orphanage/load-orphanage-controller'
+import { random, internet, address } from 'faker/locale/pt_BR'
+
+const mockOrphanage = {
+  id: random.uuid(),
+  name: internet.userName(),
+  latitude: Number(address.latitude()),
+  longitude: Number(address.longitude()),
+  about: random.words(5),
+  instructions: random.words(5),
+  opening_hours: '00:38:36',
+  closing_time: '14:38:36',
+  open_on_weekends: random.boolean()
+}
+
+const mockRequest = {
+  params: {
+    id: random.uuid()
+  },
+  body: {}
+}
+
+const mockLoadOrphanage = (): LoadOrphanage => {
+  class LoadOrphanageSpy implements LoadOrphanage {
+    loadById (id: string): Promise<Orphanage> {
+      return Promise.resolve(mockOrphanage)
+    }
+  }
+  return new LoadOrphanageSpy()
+}
+describe('Load Orphanage Controller', () => {
+  test('Should be able to call LoadOrphanage with the correct value', async () => {
+    const loadOrphanageSpy = mockLoadOrphanage()
+    const sut = new LoadOrphanageController(loadOrphanageSpy)
+    const loadById = jest.spyOn(loadOrphanageSpy, 'loadById')
+    const request = mockRequest
+    await sut.handle(request)
+    expect(loadById).toHaveBeenCalledWith(request.params.id)
+  })
+})
