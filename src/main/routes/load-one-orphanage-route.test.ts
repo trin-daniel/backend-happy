f3214cf8c13cefbc21f23d5e3a9a643fd83c6d61
trin-daniel <trindadedanniel63@outlook.com>
2020-12-04 22:35:14 -0300

    test: ensure LoadOneOrphanageRoute return an orphanage if successful

---
+++ src/main/routes/load-one-orphanage-route.test.ts
@@ -0,0 +1,30 @@
+import { AddOrphanageArgs } from '@domain/use-cases/orphanage/add-orphanage'
+import { SqlHelper } from '@infra/database/helpers/sql-helper'
+import app from '@main/config/app'
+import request from 'supertest'
+import { random, internet, address } from 'faker/locale/pt_BR'
+
+const inputs: AddOrphanageArgs = {
+  name: internet.userName(),
+  latitude: Number(address.latitude()),
+  longitude: Number(address.longitude()),
+  about: random.words(5),
+  instructions: random.words(5),
+  opening_hours: '00:38:36',
+  closing_time: '14:38:36',
+  open_on_weekends: true
+}
+
+describe('Load One Orphanage Route', () => {
+  beforeAll(async () => await SqlHelper.connect())
+  afterAll(async () => await SqlHelper.disconnect())
+  beforeEach(async () => await SqlHelper.delete('DELETE FROM orphanages'))
+
+  test('Should be able to return an orphanage if successful', async () => {
+    const id = random.uuid()
+    await SqlHelper.insertOne('INSERT INTO orphanages (id, name, latitude, longitude, about, instructions, opening_hours, closing_time, open_on_weekends)  VALUES (?,?,?,?,?,?,?,?,?)',
+      [id, inputs.name, inputs.latitude, inputs.longitude, inputs.about, inputs.instructions, inputs.opening_hours, inputs.closing_time, inputs.open_on_weekends]
+    )
+    await request(app).get(`/api/orphanage/${id}`).expect(200)
+  })
+})
