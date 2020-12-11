d672b7329b9fd30390a4a962db14d8180d517b83
trin-daniel <trindadedanniel63@outlook.com>
2020-12-10 21:35:37 -0300

    test: ensure AddImageOrphanageController call AddImageOrphanage with correct values

--- src/presentation/controllers/orphanage/add-image-orphanage/add-image-orphanage-controller.spec.ts
+++ src/presentation/controllers/orphanage/add-image-orphanage/add-image-orphanage-controller.spec.ts
@@ -5,6 +5,7 @@ import { InvalidRouteParamError } from '@presentation/errors'
 import { Image, Orphanage } from '@domain/models/orphanage'
 import { LoadOneOrphanage } from '@domain/use-cases/orphanage/load-one-orphanage'
 import { random, system, internet, address } from 'faker/locale/pt_BR'
+import { AddImageOrphanage } from '@domain/use-cases/orphanage/add-image-orphanage'
 
 const photo = (): Image => ({ id: random.uuid(), filename: system.fileName(), path: system.filePath(), destination: system.directoryPath(), mimetype: system.mimeType(), size: 256 })
 const mockRequest: HttpRequest<any> = {
@@ -37,14 +38,25 @@ const mockLoadOneOrphanage = (): LoadOneOrphanage => {
   return new LoadOneOrphanageSpy()
 }
 
-type SutTypes = { sut: AddImageOrphanageController, loadOneOrphanageSpy: LoadOneOrphanage }
+const mockAddImageOrphanageSpy = (): AddImageOrphanage => {
+  class AddImageOrphanageSpy implements AddImageOrphanage {
+    async add (files?: Image[]): Promise<void> {
+      return Promise.resolve()
+    }
+  }
+  return new AddImageOrphanageSpy()
+}
+
+type SutTypes = { sut: AddImageOrphanageController, loadOneOrphanageSpy: LoadOneOrphanage, addImageOrphanageSpy: AddImageOrphanage }
 
 const makeSut = (): SutTypes => {
+  const addImageOrphanageSpy = mockAddImageOrphanageSpy()
   const loadOneOrphanageSpy = mockLoadOneOrphanage()
-  const sut = new AddImageOrphanageController(loadOneOrphanageSpy)
+  const sut = new AddImageOrphanageController(loadOneOrphanageSpy, addImageOrphanageSpy)
   return {
     sut,
-    loadOneOrphanageSpy
+    loadOneOrphanageSpy,
+    addImageOrphanageSpy
   }
 }
 
@@ -63,5 +75,13 @@ describe('Add Image Orphanage Controller', () => {
     const request = mockRequest
     const response = await sut.handle(request)
     expect(response).toEqual(notFound(new InvalidRouteParamError('orphanage_id')))
+  })
+
+  test('Should be able to call AddImageOrphanage with correct values', async () => {
+    const { sut, addImageOrphanageSpy } = makeSut()
+    const loadById = jest.spyOn(addImageOrphanageSpy, 'add')
+    const request = mockRequest
+    await sut.handle(request)
+    expect(loadById).toHaveBeenCalledWith(request.files, request.params.orphanage_id)
   })
 })
