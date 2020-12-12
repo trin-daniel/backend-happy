3bd7e929937190edce89b11f0eaf06b94eb40bd6
trin-daniel <trindadedanniel63@outlook.com>
2020-12-11 21:39:48 -0300

    feat: ensure LoadOneOrphanageController return a 404 error if the provided identifier does not exist

--- src/presentation/controllers/orphanage/load-one-orphanage/load-one-orphanage-controller.ts
+++ src/presentation/controllers/orphanage/load-one-orphanage/load-one-orphanage-controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpRequest, HttpResponse, LoadOneOrphanage, Orphanage } from '@presentation/controllers/orphanage/load-one-orphanage/load-one-orphanage-controller-protocols'
-import { ok, serverError } from '@presentation/helpers/http-helpers'
+import { InvalidRouteParamError } from '@presentation/errors'
+import { notFound, ok, serverError } from '@presentation/helpers/http-helpers'
 
 export class LoadOneOrphanageController implements Controller {
   constructor (
@@ -9,6 +10,9 @@ export class LoadOneOrphanageController implements Controller {
   async handle (req: HttpRequest<any>): Promise<HttpResponse<Orphanage | Error>> {
     try {
       const orphanage = await this.LoadOneOrphanage.loadById(req.params.id)
+      if (!orphanage) {
+        return notFound(new InvalidRouteParamError('orphanage_id'))
+      }
       return ok(orphanage)
     } catch (error) {
       return serverError(error)
